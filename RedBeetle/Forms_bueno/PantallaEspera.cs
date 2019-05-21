992660bd224296b7af3cae2589aa6069c523dbf1
ALUMNADO\alalst <aldimirprincipal@gmail.com>
2019-05-21 15:51:31 +0200

    Creada pantalla de espera

---
+++ RedBeetle/Forms_bueno/PantallaEspera.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RedBeetle.Forms_bueno
+{
+	public partial class PantallaEspera : Form
+	{
+		public Action Proceso { get; set; }
+
+		public PantallaEspera(Action proceso)
+		{
+			InitializeComponent();
+			Proceso = proceso;
+		}
+
+		protected override void OnLoad(EventArgs e)
+		{
+			base.OnLoad(e);
+			Task.Factory.StartNew(Proceso).ContinueWith(t => { this.Close(); }, TaskScheduler.FromCurrentSynchronizationContext());
+		}
+	}
+}
